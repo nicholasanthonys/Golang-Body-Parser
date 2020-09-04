44e887f280276ca4546113b55acab7c397891932
nicholasanthonys <anthony.nicholas84@gmail.com>
2020-09-04 22:03:46 +0700

    recursive add modify and deletion map

--- internal/service/map_modifier.go
+++ internal/service/map_modifier.go
@@ -3,47 +3,79 @@ package service
 import (
 	"fmt"
 	"github.com/nicholasantnhonys/Golang-Body-Parser/internal/model"
-	"github.com/sirupsen/logrus"
+	"strings"
 )
 
-func Add(command model.Command, requestFromUser map[string]interface{}) {
-	logrus.Info("request form user is")
-	logrus.Info(requestFromUser)
-	for key, value := range command.Adds {
-		logrus.Info("Key: ", key, " Value: "+
-			"", value)
-		//* if key is not exist, add to map
-		if requestFromUser[key] == nil {
-			requestFromUser[key] = value
+func AddRecursive(listTraverse []string, value string, in interface{}, index int) interface{} {
+	if index == len(listTraverse)-1 {
+		in.(map[string]interface{})[listTraverse[index]] = value
+		return in
+	}
+	//* allocate new map if map[key] null
+	if in.(map[string]interface{})[listTraverse[index]] == nil {
+		in.(map[string]interface{})[listTraverse[index]] = make(map[string]interface{})
+	}
+	in.(map[string]interface{})[listTraverse[index]] = AddRecursive(listTraverse, value, in.(map[string]interface{})[listTraverse[index]], index+1)
+	return in.(map[string]interface{})
+}
+
+func ModifyRecursive(listTraverse []string, value string, in interface{}, index int) interface{} {
+	if index == len(listTraverse)-1 {
+		if in.(map[string]interface{})[listTraverse[index]] == nil {
+			return nil
+		}
+		in.(map[string]interface{})[listTraverse[index]] = value
+		return in
+	}
+
+	if in.(map[string]interface{})[listTraverse[index]] != nil {
+		ModifyRecursive(listTraverse, value, in.(map[string]interface{})[listTraverse[index]], index+1)
+		return in.(map[string]interface{})
+	}
+	return nil
+
+}
+
+func DeleteRecursive(listTraverse []string, in interface{}, index int) interface{} {
+	if index == len(listTraverse)-1 {
+
+		if in.(map[string]interface{})[listTraverse[index]] == nil {
+			return nil
 		}
 
+		//in.(map[string]interface{})[listTraverse[index]] = "deleted"
+		delete(in.(map[string]interface{}), listTraverse[index])
+
+		return in
 	}
+
+	if in.(map[string]interface{})[listTraverse[index]] != nil {
+		DeleteRecursive(listTraverse, in.(map[string]interface{})[listTraverse[index]], index+1)
+		return in.(map[string]interface{})
+	}
+
+	return nil
 }
 
-func Modify(command model.Command, requestFromUser map[string]interface{}) {
-	fmt.Println("Modify")
-	//*modify key from configure.json to requestFromUser Map
-	for key, value := range command.Modifies {
-		fmt.Println("Key:", key, "Value:", value)
-		if requestFromUser[key] != nil {
-			requestFromUser[key] = value
-		}
+func DoCommandConfigure(command model.Command, requestFromUser model.Fields) {
+
+	//*Do add
+	for key, value := range command.Adds.Body {
+		listTraverse := strings.Split(key, ".")
+		AddRecursive(listTraverse, fmt.Sprintf("%v", value), requestFromUser.Body, 0)
 
 	}
-}
+	//*Do Modify
+	for key, value := range command.Modifies.Body {
+		listTraverse := strings.Split(key, ".")
 
-func Delete(command model.Command, requestFromUser map[string]interface{}) {
-	//*delete dari map
-	fmt.Println("Deletes")
-	for key, value := range command.Deletes {
-		fmt.Println("key is ", key, "value is ", value)
-		delete(requestFromUser, value)
-
+		ModifyRecursive(listTraverse, fmt.Sprintf("%v", value), requestFromUser.Body, 0)
 	}
-}
 
-func DoCommandConfigure(command model.Command, requestFromUser map[string]interface{}) {
-	Add(command, requestFromUser)
-	Delete(command, requestFromUser)
-	Modify(command, requestFromUser)
+	//* Do Deletion
+	for _, key := range command.Deletes.Body {
+		listTraverse := strings.Split(key, ".")
+		DeleteRecursive(listTraverse, requestFromUser.Body, 0)
+	}
+
 }
