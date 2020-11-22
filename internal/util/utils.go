dd701e24b1bb23241bd53334fda47e0fb8209fd5
nicholasanthonys <anthony.nicholas84@gmail.com>
2020-11-22 22:35:15 +0700

    move JsonCompare byte to package util.

--- internal/util/utils.go
+++ internal/util/utils.go
@@ -1,13 +1,16 @@
-package service
+package util
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/clbanning/mxj/x2j"
 	"github.com/labstack/echo"
 	"github.com/nicholasanthonys/Golang-Body-Parser/internal/model"
 	"github.com/sirupsen/logrus"
+	"io"
 	"io/ioutil"
 	"os"
+	"reflect"
 	"strings"
 )
 
@@ -144,4 +147,31 @@ func RemoveCharacters(input string, characters string) string {
 
 	return strings.Map(filter, input)
 
+}
+
+// JSONEqual compares the JSON from two Readers.
+func JSONEqual(a, b io.Reader) (bool, error) {
+	var j, j2 interface{}
+	d := json.NewDecoder(a)
+	if err := d.Decode(&j); err != nil {
+		return false, err
+	}
+	d = json.NewDecoder(b)
+	if err := d.Decode(&j2); err != nil {
+		return false, err
+	}
+	return reflect.DeepEqual(j2, j), nil
+}
+
+// JSONBytesEqual compares the JSON in two byte slices.
+func JSONBytesEqual(a, b []byte) (bool, error) {
+	var j, j2 interface{}
+	if err := json.Unmarshal(a, &j); err != nil {
+		logrus.Error("Error unmarhsaling a")
+	}
+	if err := json.Unmarshal(b, &j2); err != nil {
+		logrus.Error("Error unmarhsaling b ")
+	}
+
+	return reflect.DeepEqual(j2, j), nil
 }
--- test/util_test.go
+++ test/util_test.go
@@ -1,7 +1,7 @@
 package test
 
 import (
-	"github.com/nicholasanthonys/Golang-Body-Parser/internal/service"
+	"github.com/nicholasanthonys/Golang-Body-Parser/internal/util"
 	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
 	"testing"
@@ -10,13 +10,13 @@ import (
 func TestFind(t *testing.T) {
 	letters := []string{"a", "b", "c", "d"}
 	expected := true
-	_, exist := service.Find(letters, "a")
+	_, exist := util.Find(letters, "a")
 	assert.Equal(t, expected, exist)
 }
 
 func TestGetListFolder(t *testing.T) {
 	expected := []string{"configure0.json", "configure1.json", "response.json"}
-	files, err := service.GetListFolder("../configures")
+	files, err := util.GetListFolder("../configures")
 	results := make([]string, 0)
 	if err != nil {
 		assert.Error(t, err, "Cannot get list folder")
