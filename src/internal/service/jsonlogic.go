8a4abfaa3f62ecfb6bb41a0dba5ac8a661ec55f6
nicholasanthonys <anthony.nicholas84@gmail.com>
2021-02-19 23:53:38 +0700

    add function to traverse json and transform it to get configure request

---
+++ src/internal/service/jsonlogic.go
@@ -0,0 +1,44 @@
+package service
+
+import (
+	"fmt"
+	"github.com/nicholasanthonys/Golang-Body-Parser/internal/model"
+	"reflect"
+	"strings"
+)
+
+func InterfaceDirectModifier(in interface{}, mapWrapper map[string]model.Wrapper, separator string) interface{} {
+
+	if in == nil {
+		return nil
+	}
+
+	it := reflect.TypeOf(in)
+
+	if it.Kind() == reflect.Map {
+
+		for index, val := range in.(map[string]interface{}) {
+			in.(map[string]interface{})[index] = InterfaceDirectModifier(val, mapWrapper, separator)
+		}
+
+	} else if it.Kind() == reflect.Slice {
+		for index, el := range in.([]interface{}) {
+			in.([]interface{})[index] = InterfaceDirectModifier(el, mapWrapper, separator)
+		}
+	} else {
+		//* if value has prefix $configure
+		if strings.HasPrefix(fmt.Sprintf("%v", in), "$configure") {
+			splittedValue := strings.Split(fmt.Sprintf("%v", in), separator) //$configure1.json, $request, $body[user][name]
+			if splittedValue[1] == "$request" {
+				val := RetrieveValue(splittedValue[2], mapWrapper[splittedValue[0]].Request)
+				in = val
+			} else {
+				val := RetrieveValue(splittedValue[2], mapWrapper[splittedValue[0]].Response)
+				in = val
+			}
+		}
+	}
+
+	return in
+
+}
