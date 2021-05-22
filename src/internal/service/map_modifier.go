b0599e9edb57abda26a05eb341cb8d37d489458c
nicholasanthonys <anthony.nicholas84@gmail.com>
2021-05-22 22:48:18 +0700

    refactor code : add function GetFromFullReferenceValue and GetFromHalfReferenceValue

--- src/internal/service/map_modifier.go
+++ src/internal/service/map_modifier.go
@@ -1,8 +1,8 @@
 package service
 
 import (
+	"errors"
 	"fmt"
-	"github.com/jinzhu/copier"
 	"github.com/nicholasanthonys/Golang-Body-Parser/internal/model"
 	"github.com/nicholasanthonys/Golang-Body-Parser/internal/util"
 	cmap "github.com/orcaman/concurrent-map"
@@ -31,17 +31,9 @@ func AddRecursive(listTraverse []string, value interface{}, in interface{}, inde
 
 	if index == len(listTraverse)-1 {
 		if fmt.Sprintf("%v", reflect.TypeOf(in)) == "map[string]interface {}" {
-
-			//*only add when the value of the key is null
-			//mutex.Lock()
 			if in.(map[string]interface{})[listTraverse[index]] == nil {
-				tmpInterface := make(map[string]interface{})
-				tmp := in.(map[string]interface{})
-				copier.Copy(&tmpInterface, &tmp)
 
-				tmpInterface[listTraverse[index]] = value
-				copier.Copy(&in, &tmpInterface)
-
+				in.(map[string]interface{})[listTraverse[index]] = value
 			}
 		}
 
@@ -49,14 +41,13 @@ func AddRecursive(listTraverse []string, value interface{}, in interface{}, inde
 	}
 
 	if fmt.Sprintf("%v", reflect.TypeOf(in)) == "map[string]interface {}" {
-		//* allocate new map if map[key] null
-		//mutex.Lock()
+		// allocate new map if map[key] null
+
 		if in.(map[string]interface{})[listTraverse[index]] == nil {
 			in.(map[string]interface{})[listTraverse[index]] = make(map[string]interface{})
 		}
-		//mutex.Unlock()
 
-		//* recursively traverse the map
+		// recursively traverse the map
 		in.(map[string]interface{})[listTraverse[index]] = AddRecursive(listTraverse, value, in.(map[string]interface{})[listTraverse[index]], index+1)
 
 		return in.(map[string]interface{})
@@ -74,14 +65,10 @@ func ModifyRecursive(listTraverse []string, value interface{}, in interface{}, i
 			if in.(map[string]interface{})[listTraverse[index]] == nil {
 				return nil
 			}
-			tmpInterface := make(map[string]interface{})
-			tmp := in.(map[string]interface{})
-			copier.Copy(&tmpInterface, &tmp)
 
-			tmpInterface[listTraverse[index]] = value
-			copier.Copy(&in, &tmpInterface)
-			//in.(map[string]interface{})[listTraverse[index]] = value
+			in.(map[string]interface{})[listTraverse[index]] = value
 		}
+
 		if fmt.Sprintf("%v", reflect.TypeOf(in)) == "[]interface {}" {
 			realIndex, err := strconv.Atoi(listTraverse[index])
 			if err == nil {
@@ -145,89 +132,70 @@ func DeleteRecursive(listTraverse []string, in interface{}, index int) interface
 	return in
 }
 
-// RetrieveValue is a function that check the value type value from configure and retrieve the value from header,body, or query
-func RetrieveValue(value interface{}, takeFrom cmap.ConcurrentMap, loopIndex int) interface{} {
-	//*declare empty result
+// GetFromHalfReferenceValue is a function that check the value type value from configure and retrieve the value from header,body, or query
+func GetFromHalfReferenceValue(value interface{}, takeFrom cmap.ConcurrentMap, loopIndex int) interface{} {
+	// declare empty result
 	var realValue interface{}
-	//* check the type of the value
+	// check the type of the value
 	vt := reflect.TypeOf(value).Kind()
 
-	if reflect.String == vt {
-		//* We Call Sanitizevalue to clear the value from the square bracket and the Dollar Sign
-		listTraverseVal, destination := util.SanitizeValue(fmt.Sprintf("%v", value))
+	if reflect.String != vt {
+		log.Error("reference value type is not string. for value : ", value)
+		return value
+	}
 
-		if len(destination) == 0 {
-			log.Info("destination not found, returning : ", realValue)
+	// We Call GetListTraverseAndDestination to clear the value from the square bracket and the Dollar Sign, and get list traverse and destination
+	listTraverseVal, destination := util.GetListTraverseAndDestination(fmt.Sprintf("%v", value))
 
-			return realValue
-		}
-		if listTraverseVal != nil {
-			var key string
-			if destination == "body" {
-				key = "body"
-			}
-			if destination == "header" {
-				key = "header"
-			}
-			if destination == "query" {
-				key = "query"
-			}
-			if destination == "path" {
-				key = "param"
-			}
-			if destination == "status_code" {
-				key = "statusCode"
-			}
-			if tmp, ok := takeFrom.Get(key); ok {
-				if key != "statusCode" {
-					mutex.Lock()
-					tmpMap := tmp.(map[string]interface{})
+	if len(destination) == 0 || listTraverseVal == nil {
+		return realValue
+	}
 
-					realValue = recursiveGetValue(listTraverseVal, tmpMap, 0, loopIndex)
-					mutex.Unlock()
-				} else {
-					mutex.Lock()
-					vt := reflect.TypeOf(tmp)
-					if vt.Kind() == reflect.String {
-						tmp, _ = strconv.Atoi(tmp.(string))
-					}
-					tmpMap := tmp.(int)
-					realValue = recursiveGetValue(listTraverseVal, tmpMap, 0, loopIndex)
-					mutex.Unlock()
-				}
+	if tmp, ok := takeFrom.Get(destination); ok {
+		if destination != "statusCode" {
+			mutex.Lock()
+			tmpMap := tmp.(map[string]interface{})
 
-			}
+			realValue = recursiveGetValue(listTraverseVal, tmpMap, 0, loopIndex)
+			mutex.Unlock()
 		} else {
-			realValue = value
+			mutex.Lock()
+			vt := reflect.TypeOf(tmp)
+			if vt.Kind() == reflect.String {
+				tmp, _ = strconv.Atoi(tmp.(string))
+			}
+			tmpMap := tmp.(int)
+			realValue = recursiveGetValue(listTraverseVal, tmpMap, 0, loopIndex)
+			mutex.Unlock()
 		}
 
 	} else {
-
-		realValue = value
+		log.Error("cannot get reference value destination for value ", value)
 	}
+
 	if realValue == nil {
-		//* if value is not found, return empty string
+		// if value is not found, return empty string
 		return ""
 	}
 	return realValue
 }
 
-//* recursiveGetValue is a function that will recursively traverse the whole map
-//* get the value based on the listTraverse
+// recursiveGetValue is a function that will recursively traverse the whole map
+// get the value based on the listTraverse
 func recursiveGetValue(listTraverse []string, in interface{}, index int, loopIndex int) interface{} {
-
 	if len(listTraverse) > 0 {
 		if index == len(listTraverse)-1 {
 			//*check the type of the target
 			rt := reflect.TypeOf(in)
+
 			switch rt.Kind() {
 			case reflect.Slice:
 				var indexInt int
 				var err error
 
-				//*check type slice element
-				//* example :  $body[user][name][0]. Now we have the 0 as index type string. we need to
-				//* convert the 0 to become integer
+				// check type slice element
+				// example :  $body[user][name][0]. Now we have the 0 as index type string. we need to
+				// convert the 0 to become integer
 
 				// handle case if index is loop, ex $body[user][loop]
 				if listTraverse[index] == "loop" {
@@ -241,9 +209,8 @@ func recursiveGetValue(listTraverse []string, in interface{}, index int, loopInd
 					return nil
 				}
 
-				//*if the type of the interface is slice
+				//if the type of the interface is slice
 				if len(in.([]interface{})) > indexInt {
-
 					return in.([]interface{})[indexInt]
 				}
 				return nil
@@ -258,13 +225,13 @@ func recursiveGetValue(listTraverse []string, in interface{}, index int, loopInd
 			}
 		}
 
-		//*if the type is map, we need to traverse recursively again
+		//if the type is map, we need to traverse recursively again
 		if fmt.Sprintf("%v", reflect.TypeOf(in)) == "map[string]interface {}" {
-			//*if the map is nil, return interface
+			//if the map is nil, return interface
 			if in.(map[string]interface{})[listTraverse[index]] == nil {
 				return nil
 			}
-			//* recursively traverse the map again
+			// recursively traverse the map again
 			return recursiveGetValue(listTraverse, in.(map[string]interface{})[listTraverse[index]], index+1, loopIndex)
 		} else {
 			return nil
@@ -274,13 +241,10 @@ func recursiveGetValue(listTraverse []string, in interface{}, index int, loopInd
 	return in
 }
 
-//*DoAddModifyDelete is a function that will do the command from configure.json for Header, Query, and Body
-//* Here, we call DoCommandConfigure for each Header, Query, and Body
-//* fields is field that want to be modify
+// DoAddModifyDelete is a function that will do the command from configure.json for Header, Query, and Body
+// Here, we call DoCommandConfigure for each Header, Query, and Body
+// fields is field that want to be modify
 func DoAddModifyDelete(command model.Command, fields *cmap.ConcurrentMap, takeFrom *cmap.ConcurrentMap, loopIndex int) map[string]interface{} {
-	//tmpHeader := make(map[string]interface{})
-	//tmpBody := make(map[string]interface{})
-	//tmpQuery := make(map[string]interface{})
 	var fieldHeader map[string]interface{}
 	var fieldQuery map[string]interface{}
 	var fieldBody map[string]interface{}
@@ -288,32 +252,22 @@ func DoAddModifyDelete(command model.Command, fields *cmap.ConcurrentMap, takeFr
 	//*header
 	if tmp, ok := fields.Get("header"); ok {
 		fieldHeader = tmp.(map[string]interface{})
-
 		fieldHeader = AddToWrapper(command.Adds.Header, "--", fieldHeader, takeFrom, loopIndex)
-		//*modify header
 		fieldHeader = ModifyWrapper(command.Modifies.Header, "--", fieldHeader, takeFrom, loopIndex)
-		//*Deletion Header
 		fieldHeader = DeletionHeaderOrQuery(command.Deletes.Header, fieldHeader)
 	}
 
 	if tmp, ok := fields.Get("query"); ok {
 		fieldQuery = tmp.(map[string]interface{})
-		//* Add Query
 		fieldQuery = AddToWrapper(command.Adds.Query, "--", fieldQuery, takeFrom, loopIndex)
-		//*modify Query
 		fieldQuery = ModifyWrapper(command.Modifies.Query, "--", fieldQuery, takeFrom, loopIndex)
-		//*Deletion Query
 		fieldQuery = DeletionHeaderOrQuery(command.Deletes.Query, fieldQuery)
 	}
 
 	if tmp, ok := fields.Get("body"); ok {
 		fieldBody = tmp.(map[string]interface{})
-
-		//* add body
 		fieldBody = AddToWrapper(command.Adds.Body, "--", fieldBody, takeFrom, loopIndex)
-		//*modify body
 		fieldBody = ModifyWrapper(command.Modifies.Body, "--", fieldBody, takeFrom, loopIndex)
-		//*deletion to body
 		fieldBody = DeletionBody(command.Deletes, fieldBody)
 	}
 	return map[string]interface{}{
@@ -324,7 +278,7 @@ func DoAddModifyDelete(command model.Command, fields *cmap.ConcurrentMap, takeFr
 }
 
 func DeletionBody(deleteField model.DeleteFields, mapKeyToBeRemoved map[string]interface{}) map[string]interface{} {
-	//* Do Deletion
+	// Do Deletion
 	for _, key := range deleteField.Body {
 		listTraverse := strings.Split(key, ".")
 		mutex.Lock()
@@ -346,97 +300,79 @@ func DeletionHeaderOrQuery(deleteField []string, mapToBeDeleted map[string]inter
 }
 
 func ModifyPath(path string, separator string, takeFrom *cmap.ConcurrentMap, loopIndex int) string {
-	//*example, what we got here is like this
-	//* /person/{{$configure1.json--$request--$body[user][name]/transaction/{{$configure1.json--$request--$body[user][name]}}
-	//* we need to split based from separator /, and looping and find if there is {{ }}
+	// example, what we got here is like this :
+	// person/{{$configure1.json--$request--$body[user][name]/transaction/{{$configure1.json--$request--$body[user][name]}}
+	// we need to split based from separator /, and looping and find if there is {{ }}
 	splittedPath := strings.Split(path, "/")
 	for _, val := range splittedPath {
 		if strings.Contains(val, "{{") && strings.Contains(val, "}}") {
-			removedBracket := util.RemoveCharacters(val, "{{}}")
+			valWithoutBracket := util.RemoveCharacters(val, "{{}}")
 
-			//*split value : $configure1.json-$request-$body[user][name]
-			var realValue interface{}
-			//* if value has prefix $configure
-			if strings.HasPrefix(fmt.Sprintf("%v", removedBracket), "$configure") {
-				splittedValue := strings.Split(fmt.Sprintf("%v", removedBracket), separator) //$configure1.json, $request, $body[user][name]
-				if len(splittedValue) != 3 {
-					log.Error("referenced syntax wrong for : ", removedBracket)
-					log.Error(splittedValue)
-					return ""
-				}
-
-				//remove dollar sign
-				var wrapper *model.Wrapper
-				if tmp, ok := takeFrom.Get(splittedValue[0]); ok {
-					wrapper = tmp.(*model.Wrapper)
-				} else {
-					return ""
-				}
-				if splittedValue[1] == "$request" {
-					//* get the request from fields
-					realValue = RetrieveValue(splittedValue[2], wrapper.Request, loopIndex)
-				} else {
-					//* get the response from fields
-					realValue = RetrieveValue(splittedValue[2], wrapper.Response, loopIndex)
-				}
-
-				if realValue != nil {
-
-					vt := reflect.TypeOf(realValue).Kind()
-					if reflect.String == vt {
-
-						path = strings.Replace(path, val, realValue.(string), -1)
-					}
-
-				} else {
-					log.Info("real value for path is nil, returning empty string")
-					return ""
-				}
-
+			realValue, err := GetFromFullReferenceValue(separator, takeFrom, loopIndex, valWithoutBracket)
+			if err != nil {
+				log.Error("from function modifyPath. Error is ", err.Error())
+				return path
 			}
+			if realValue != nil {
+				vt := reflect.TypeOf(realValue).Kind()
+				if reflect.String == vt {
+					// replace path with pattern splittedPath value with real value
+					path = strings.Replace(path, val, realValue.(string), -1)
+					return path
+				}
+			}
+			log.Error("real value for path is nil, returning original path")
+			return path
 
 		}
 	}
 
 	return path
-
 }
 
-//*AddToWrapper is a function that will add value to the specified key to a map
+//GetFromFullReferenceValue is a function that will get value from full reference like $configure--first--configure--$body[user][name]
+func GetFromFullReferenceValue(separator string, takeFrom *cmap.ConcurrentMap, loopIndex int, fullReferenceValue interface{}) (interface{}, error) {
+	var realValue interface{}
+
+	// if value has prefix $configure
+	if strings.HasPrefix(fmt.Sprintf("%v", fullReferenceValue), "$configure") {
+		splittedValue := strings.Split(fmt.Sprintf("%v", fullReferenceValue), separator) //$configure1.json, $request, $body[user][name]
+		if len(splittedValue) != 3 {
+			log.Error("referenced syntax wrong for : ", fullReferenceValue)
+			log.Error(splittedValue)
+			return nil, errors.New("referenced syntax wrong for : " + fullReferenceValue.(string))
+		}
+
+		var wrapper *model.Wrapper
+		if tmp, ok := takeFrom.Get(splittedValue[0]); ok {
+			wrapper = tmp.(*model.Wrapper)
+			if splittedValue[1] == "$request" {
+				// get the request from fields
+				realValue = GetFromHalfReferenceValue(splittedValue[2], wrapper.Request, loopIndex)
+				return realValue, nil
+			}
+			// get the response from fields
+			realValue = GetFromHalfReferenceValue(splittedValue[2], wrapper.Response, loopIndex)
+			return realValue, nil
+		}
+
+		// return empty reference
+		return realValue, nil
+
+	}
+
+	return fullReferenceValue, nil
+}
+
+// AddToWrapper is a function that will add value to the specified key to a map
 func AddToWrapper(commands map[string]interface{}, separator string, mapToBeAdded map[string]interface{}, takeFrom *cmap.ConcurrentMap, loopIndex int) map[string]interface{} {
-	//* Add key
 	for key, value := range commands {
-		//*get the value
-		//*split value : $configure1.json-$request-$body[user][name]
-		var realValue interface{}
-		//* if value has prefix $configure
-		if strings.HasPrefix(fmt.Sprintf("%v", value), "$configure") {
-			splittedValue := strings.Split(fmt.Sprintf("%v", value), separator) //$configure1.json, $request, $body[user][name]
-			if len(splittedValue) != 3 {
-				log.Error("referenced syntax wrong for : ", value)
-				log.Error(splittedValue)
-				return mapToBeAdded
-			}
-
-			//remove dollar sign
-			//splittedValue[0] = util.RemoveCharacters(splittedValue[0], "$")
-			var wrapper *model.Wrapper
-			if tmp, ok := takeFrom.Get(splittedValue[0]); ok {
-				wrapper = tmp.(*model.Wrapper)
-				if splittedValue[1] == "$request" {
-					//* get the request from fields
-					realValue = RetrieveValue(splittedValue[2], wrapper.Request, loopIndex)
-				} else {
-					//* get the response from fields
-					realValue = RetrieveValue(splittedValue[2], wrapper.Response, loopIndex)
-				}
-			}
-
-		} else {
-			realValue = value
+		// get real value from full reference value like $configure1.json-$request-$body[user][name]
+		realValue, err := GetFromFullReferenceValue(separator, takeFrom, loopIndex, value)
+		if err != nil {
+			return mapToBeAdded
 		}
 		listTraverseKey := strings.Split(key, ".")
-
 		mutex.Lock()
 		mapToBeAdded = AddRecursive(listTraverseKey, realValue, mapToBeAdded, 0).(map[string]interface{})
 		mutex.Unlock()
@@ -444,41 +380,15 @@ func AddToWrapper(commands map[string]interface{}, separator string, mapToBeAdde
 	return mapToBeAdded
 }
 
-//*ModifyWrapper is a function that will modify value based from specific key
+// ModifyWrapper is a function that will modify value based from specific key
 func ModifyWrapper(commands map[string]interface{}, separator string, mapToBeModified map[string]interface{}, takeFrom *cmap.ConcurrentMap, loopIndex int) map[string]interface{} {
 	for key, value := range commands {
-		var realValue interface{}
-		//* if value has prefix $configurex.json
-		if strings.HasPrefix(fmt.Sprintf("%v", value), "$configure") {
-			//* split : $configure1.json-$request-$body[user]
-			//* into $configure1.json, $request, $body[user]
-			splittedValue := strings.Split(fmt.Sprintf("%v", value), separator) //$configure1.json, $request, $body[user][name]
-			if len(splittedValue) != 3 {
-				log.Error("referenced syntax wrong for : ", value)
-				log.Error(splittedValue)
-				return mapToBeModified
-			}
-			////remove dollar sign from $configure
-			//splittedValue[0] = util.RemoveCharacters(splittedValue[0], "$")
-			var wrapper *model.Wrapper
-			if tmp, ok := takeFrom.Get(splittedValue[0]); ok {
-				wrapper = tmp.(*model.Wrapper)
-			}
-			if splittedValue[1] == "$request" {
-				//* get the request from fields
-				realValue = RetrieveValue(splittedValue[2], wrapper.Request, loopIndex)
-			} else {
-				//* get the response from fields
-				realValue = RetrieveValue(splittedValue[2], wrapper.Response, loopIndex)
-			}
-
-		} else {
-			realValue = value
+		realValue, err := GetFromFullReferenceValue(separator, takeFrom, loopIndex, value)
+		if err != nil {
+			return mapToBeModified
 		}
-
 		listTraverseKey := strings.Split(key, ".")
 		mutex.Lock()
-
 		result := ModifyRecursive(listTraverseKey, realValue, mapToBeModified, 0)
 		if result != nil {
 			mapToBeModified = result.(map[string]interface{})
