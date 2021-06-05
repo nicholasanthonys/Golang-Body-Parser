4afadbd978a9ebc78b47bb66732cb6ba7996f4ea
nicholasanthonys <anthony.nicholas84@gmail.com>
2021-06-05 19:37:21 +0700

    add test case 4.3

--- src/test/case_4_test.go
+++ src/test/case_4_test.go
@@ -119,3 +119,50 @@ func TestSingleSerial_First_Request_True_Final_Logic_Refer_To_Second_Request(t *
 	}
 
 }
+
+func TestSingleSerial_First_Request_False_Logic_Refer_To_Second_Request(t *testing.T) {
+	projectDir := dirName + "/test-4.3"
+
+	req, _ := http.NewRequest("GET", URL+"/test-4-3?movie_id=550&movie_id2=384018", nil)
+	req.Header.Set("Content-Type", "application/json")
+
+	client := http.Client{}
+
+	res, err := client.Do(req)
+	if err != nil {
+		t.Errorf("Expected nil, received %s", err.Error())
+	}
+
+	assert.Equalf(t, http.StatusOK, res.StatusCode, " Expected %s but got %s", http.StatusOK, res.StatusCode)
+
+	responseByte, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		assert.Error(t, err, "should not error")
+	}
+
+	expectedByte := util.ReadJsonFile(projectDir + "/test-4.3_expected.json")
+	equal, err := util.JSONBytesEqual(expectedByte, responseByte)
+	if err != nil {
+		assert.Error(t, err, "error compare json byte")
+	}
+	if !equal {
+		var jsonExpectedPretty bytes.Buffer
+		var jsonResponseBodyPretty bytes.Buffer
+		err := json.Indent(&jsonExpectedPretty, expectedByte, "", "\t")
+		if err != nil {
+			assert.Error(t, err, "should not error")
+		}
+
+		err = json.Indent(&jsonResponseBodyPretty, responseByte, "", "\t")
+		if err != nil {
+			assert.Error(t, err, "should not error")
+		}
+
+		log.Infof("expected : ")
+		fmt.Printf("%s \n", jsonExpectedPretty.Bytes())
+		log.Infof("actual : ")
+		fmt.Printf("%s \n", jsonResponseBodyPretty.Bytes())
+		assert.Equal(t, string(jsonExpectedPretty.Bytes()), string(jsonResponseBodyPretty.Bytes()), "should be equal")
+	}
+
+}
