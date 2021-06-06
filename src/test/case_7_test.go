763ffb8d6eeaba8f83537eb418498625c1b16499
nicholasanthonys <anthony.nicholas84@gmail.com>
2021-06-06 18:57:12 +0700

    add test case 7.1

---
+++ src/test/case_7_test.go
@@ -0,0 +1,59 @@
+package test
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/nicholasanthonys/Golang-Body-Parser/internal/util"
+	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"net/http"
+	"testing"
+)
+
+func TestParallel_Logic_True_Refer_To_Another_Request(t *testing.T) {
+	projectDir := dirName + "/test-7.1"
+
+	req, _ := http.NewRequest("POST", URL+"/test-7-1", nil)
+	req.Header.Set("Content-Type", "application/json")
+
+	client := http.Client{}
+
+	res, err := client.Do(req)
+	if err != nil {
+		t.Errorf("Expected nil, received %s", err.Error())
+	}
+
+	assert.Equalf(t, http.StatusCreated, res.StatusCode, " Expected %s but got %s", http.StatusCreated,
+		res.StatusCode)
+
+	responseByte, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		assert.Error(t, err, "should not error")
+	}
+
+	expectedByte := util.ReadJsonFile(projectDir + "/test-7.1_expected.json")
+	equal, err := util.JSONBytesEqual(expectedByte, responseByte)
+	if err != nil {
+		assert.Error(t, err, "error compare json byte")
+	}
+	if !equal {
+		var jsonExpectedPretty bytes.Buffer
+		var jsonResponseBodyPretty bytes.Buffer
+		err := json.Indent(&jsonExpectedPretty, expectedByte, "", "\t")
+		if err != nil {
+			assert.Error(t, err, "should not error")
+		}
+
+		err = json.Indent(&jsonResponseBodyPretty, responseByte, "", "\t")
+		if err != nil {
+			assert.Error(t, err, "should not error")
+		}
+
+		log.Infof("expected : ")
+		fmt.Printf("%s \n", jsonExpectedPretty.Bytes())
+		log.Infof("actual : ")
+		fmt.Printf("%s \n", jsonResponseBodyPretty.Bytes())
+		assert.Equal(t, string(jsonExpectedPretty.Bytes()), string(jsonResponseBodyPretty.Bytes()), "should be equal")
+	}
+}
